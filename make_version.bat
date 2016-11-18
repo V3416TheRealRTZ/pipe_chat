11bdcee12da3ff3e68ef9b026bed97fe666c7c7a
Lo6a4evskiy <Lo6a4evskiy@gmail.com>
2016-11-19 00:32:44 +0400

    Updated version creating script to include commit hash

--- make_version.bat
+++ make_version.bat
@@ -1,4 +1,3 @@
-@echo off
 type version
 set /p Build=<version
 set /a Build+=1
@@ -6,5 +5,7 @@ set /a Build+=1
 For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%a)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 >>version echo %mydate%_%mytime%
-COPY %~dp0\client\debug\PipeChatClient.exe %~dp0\%Build%_%mydate%_%mytime%_client.exe
-COPY %~dp0\server\debug\PipeChatServer.exe %~dp0\%Build%_%mydate%_%mytime%_server.exe
+for /f %%v in ('git rev-parse --short HEAD') do set hash=%%v
+>>version echo %hash%
+COPY %~dp0\client\debug\PipeChatClient.exe "%~dp0\%Build%_%mydate%_%mytime%_%hash%_client.exe"
+COPY %~dp0\server\debug\PipeChatServer.exe "%~dp0\%Build%_%mydate%_%mytime%_%hash%_server.exe"
